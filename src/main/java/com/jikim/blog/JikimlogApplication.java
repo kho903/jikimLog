51b3138f1d527fdc05479da034a46a5deac76852
Kim Ji Hun <gmldnr2222@naver.com>
2023-08-21 13:54:03 +0900

    conf: start jikimlog project
    spring boot 2.5.15

---
+++ src/main/java/com/jikim/blog/JikimlogApplication.java
@@ -0,0 +1,13 @@
+package com.jikim.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikimlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JikimlogApplication.class, args);
+	}
+
+}
