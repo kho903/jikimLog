6635e7c86b4e05db70ad729d005aae55610d0ea2
kho903 <gmldnr2222@naver.com>
2023-08-28 15:00:20 +0900

    test: PostServiceTest
    - 글 작성 테스트

---
+++ src/test/java/com/jikim/blog/service/PostServiceTest.java
@@ -0,0 +1,42 @@
+package com.jikim.blog.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.jikim.blog.domain.Post;
+import com.jikim.blog.repository.PostRepository;
+import com.jikim.blog.request.PostCreate;
+
+@SpringBootTest
+class PostServiceTest {
+
+	@Autowired
+	private PostService postService;
+
+	@Autowired
+	private PostRepository postRepository;
+
+	@Test
+	@DisplayName("글 작성")
+	void postWrite() throws Exception {
+	    // given
+		PostCreate postCreate = PostCreate.builder()
+			.title("제목입니다.")
+			.content("내용입니다.")
+			.build();
+
+		// when
+		postService.write(postCreate);
+
+	    // then
+		assertEquals(1L, postRepository.count());
+
+		Post post = postRepository.findAll().get(0);
+		assertEquals("제목입니다.", post.getTitle());
+		assertEquals("내용입니다.", post.getContent());
+	}
+}
